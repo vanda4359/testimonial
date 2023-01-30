0324e1df0982f14202b52c0accbd6f5edac30038
Van Dang Anh <vandait996@gmail.com>
2023-01-30 16:34:42 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Testimonial from "./components/testimonial/Testimonial";
+
+function App() {
+  return (
+    <div>
+      <Testimonial />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/testimonial/data.js
@@ -0,0 +1,20 @@
+export const users = [
+    {
+        img: "https://znews-photo.zingcdn.me/w660/Uploaded/kbd_pilk/2022_09_22/vuong_hieu_than8.jpg",
+        name: "Tanya Sinclair",
+        Job: "UX Engineer",
+        desc: "Each person must have the ideal partner for themselves. For me, my ideal partner is a good heart, honesty, and respect parents, hardworking, and educated. Moreover, she is also a pretty and attractive woman.",
+    },
+    {
+        img: "https://allimages.sgp1.digitaloceanspaces.com/tipeduvn/2022/07/1657898015_604_Top-Nhung-Hinh-Anh-Girl-Xinh-De-Thuong-Nhat-Viet.jpg",
+        name: "John Tarkpor",
+        Job: "Front-end Developerr",
+        desc: "Each person must have the ideal partner for themselves. For me, my ideal partner is a good heart, honesty, and respect parents, hardworking, and educated. Moreover, she is also a pretty and attractive woman.",
+    },
+    {
+        img: "https://haycafe.vn/wp-content/uploads/2022/02/Anh-gai-xinh-Viet-Nam.jpg",
+        name: "Tanya Sinclair",
+        Job: "UX Engineer",
+        desc: "Each person must have the ideal partner for themselves. For me, my ideal partner is a good heart, honesty, and respect parents, hardworking, and educated. Moreover, she is also a pretty and attractive woman.",
+    },
+]
